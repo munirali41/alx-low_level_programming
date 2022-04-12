95044652293b6ad0ec228faaa45f480314f83cf5
sibeweyh <muniralitechse@gmail.com>
2022-04-12 16:05:57 -0700

    print lower case characters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+inlcude "main.h"
+
+/**
+  * print_alphabet - prints lower case alphabets
+  *
+  * Return: void
+  */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}
