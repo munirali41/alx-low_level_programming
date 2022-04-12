74c06d3a5603b049ede69f5571015550b757f162
sibeweyh <muniralitechse@gmail.com>
2022-04-12 16:17:55 -0700

    print the alpahbet 10 times 1

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,27 +1,25 @@
 include "main.h"
 
 /**
- * print_alphabet_x10 - print alphabet
+ * print_alphabet_x10 - printS alphabet 10 times
  *
  * Return: Always 0.
  */
 void print_alphabet_x10(void)
 {
-int alphabet;
-int count;
+	char c;
+	int i = 0;
 
+	while (i <= 9)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
 
-count = 0;
-while (count < 10)
-{
-for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
-{
-_putchar(alphabet);
+		_putchar('\n');
+
+		i++;
+	}
 }
 
-count++;
-_putchar('\n');
-}
-
-
-}
